60d568360b1d8ca61823153d48ed68a1328ac374
Kyle <kyleliu210@gmail.com>
2022-06-14 01:18:26 -0700

    implemented studios

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const bodyParser = require('body-parser'); // body parser helps access input elements from server
 
 const indexRouter = require('./routes/index');
-const animeRouter = require('./routes/anime');
+const studioRouter = require('./routes/studios');
 
 app.use(express.static(path.join(__dirname, '/public/'))); // loads files in public
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false})) 
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); // middleware to access views
 
 
 app.use('/', indexRouter);
-app.use('/anime', animeRouter);
+app.use('/studios', studioRouter);
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
