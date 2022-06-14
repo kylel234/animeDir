40a2b956dea8cfe25ce6d455b38d5645656a93c2
Kyle <kyleliu210@gmail.com>
2022-06-13 18:49:55 -0700

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {title: "myAnime!"});
+    res.render('index', {title: "myAnime! sadasd"});
 });
 
 module.exports = router;
