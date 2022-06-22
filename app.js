f1ea531cbece4d0d5adb7a71579ead80e3cede7b
Kyle <kyleliu210@gmail.com>
2022-06-21 22:55:18 -0700

    finish backend 2.0

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const animeRouter = require('./routes/animes');
 app.use(express.static(path.join(__dirname, '/public/'))); // loads files in public
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false})) 
 
-app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); // middleware to access views
+app.set('view engine', 'ejs');
+
 
 app.use(methodOverride('_method'))
 app.use('/', indexRouter);
